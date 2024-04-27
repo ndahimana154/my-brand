723cbed56500dc6956c3ff73388b718a015e7190
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-04-27 11:41:42 +0200

    Not found
    Added the not found

--- backend/src/routes/mesagesRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import messageController from "../modules/messages/controller/messageController";
-
-const messagesRouter = express.Router();
-
-// Post the message
-messagesRouter.post("/", messageController.postMessage);
-
-export default messagesRouter;
